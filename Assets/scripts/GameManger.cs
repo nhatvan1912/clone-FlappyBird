7e61628e50cf0e091c93b17c75e6dba9930bdfcd
nhatvan1912 <112192624+nhatvan1912@users.noreply.github.com>
2024-08-20 21:44:13 +0700

    first commit

---
+++ Assets/scripts/GameManger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManger : MonoBehaviour
+{
+    public static GameManger instance;
+    [SerializeField] private GameObject _gameOverCanvas;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        Time.timeScale=1f;
+    }
+    public void GameOver()
+    {
+        _gameOverCanvas.SetActive(true);
+        Time.timeScale=0f;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+}
